a8d172246b50f976dc5edea6e908822c451ff50e
Alex Lezard <alex.lezard@ocado.com>
2024-01-08 10:23:41 +0000

    l and h functionality with nvimtree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,80 +9,78 @@ return {
     { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle tree" },
   },
   --
-  config = function()
-     require("nvim-tree").setup {}
-  end,
-}
---   config = function ()
---   vim.g.loaded_netrw = 1
---   vim.g.loaded_netrwPlugin = 1
---
---   local status_ok, nvim_tree = pcall(require, "nvim-tree")
---   if not status_ok then
---     return
---   end
---
---   local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
---   if not config_status_ok then
---     return
---   end
---
---   local tree_cb = nvim_tree_config.nvim_tree_callback
---
---   return require("nvim-tree").setup {
---        update_focused_file = {
---           enable = true,
---           update_cwd = true,
---         },
---         renderer = {
---           root_folder_modifier = ":t",
---           icons = {
---             glyphs = {
---               default = "",
---               symlink = "",
---               folder = {
---                 arrow_open = "",
---                 arrow_closed = "",
---                 default = "",
---                 open = "",
---                 empty = "",
---                 empty_open = "",
---                 symlink = "",
---                 symlink_open = "",
---               },
---               git = {
---                 unstaged = "",
---                 staged = "S",
---                 unmerged = "",
---                 renamed = "➜",
---                 untracked = "U",
---                 deleted = "",
---                 ignored = "◌",
---               },
---             },
---           },
---         },
---         diagnostics = {
---           enable = true,
---           show_on_dirs = true,
---           icons = {
---             hint = "",
---             info = "",
---             warning = "",
---             error = "",
---           },
---         },
---         view = {
---           width = 30,
---           side = "left",
---          --   mappings = {
---          --        list = {
---          --          { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
---          --          { key = "h", cb = tree_cb "close_node" },
---          --          { key = "v", cb = tree_cb "vsplit" },
---          --  },
---          -- },
---         },
---       }
---   end
+  -- config = function()
+     -- require("nvim-tree").setup {}
+  -- end,
 -- }
+  config = function ()
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  local function my_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+    vim.keymap.set('n', 'l',     api.node.open.edit,                  opts('Open'))
+    vim.keymap.set('n', 'h',     api.node.navigate.parent_close,     opts('Close'))
+  end
+
+  require("nvim-tree").setup {
+       on_attach = my_on_attach,
+       update_focused_file = {
+          enable = true,
+          update_cwd = true,
+        },
+        renderer = {
+          root_folder_modifier = ":t",
+          icons = {
+            glyphs = {
+              default = "",
+              symlink = "",
+              folder = {
+                arrow_open = "",
+                arrow_closed = "",
+                default = "",
+                open = "",
+                empty = "",
+                empty_open = "",
+                symlink = "",
+                symlink_open = "",
+              },
+              git = {
+                unstaged = "",
+                staged = "S",
+                unmerged = "",
+                renamed = "➜",
+                untracked = "U",
+                deleted = "",
+                ignored = "◌",
+              },
+            },
+          },
+        },
+        diagnostics = {
+          enable = true,
+          show_on_dirs = true,
+          icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+          },
+        },
+        view = {
+          width = 30,
+          side = "left",
+        },
+      }
+  end
+}
