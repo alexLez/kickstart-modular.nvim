aa3019eefe6a2ca42c5c00c0301f55483a818f03
Alex Lezard <alex.lezard@ocado.com>
2024-01-08 17:53:42 +0000

    Keep visual selection on indentation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,10 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Reselect text after indentation
+vim.keymap.set('v', '>', ">gv")
+vim.keymap.set('v', '<', "<gv")
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
